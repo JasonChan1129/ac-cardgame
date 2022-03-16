53d231b68f3305b6386586bf8d8e8ac7f7d08d38
Jason Chan <jasonchan19981017@hotmail.com>
2022-03-16 19:00:39 +0800

    fix bugs in pairCards

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const view = {
 	},
 	pairCards(...cards) {
 		cards.forEach(card => {
-			card.classList.add('paired');
+			card.querySelector('.front').classList.add('paired');
 		});
 	},
 	renderScore(score) {
